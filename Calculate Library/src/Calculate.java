e61176329fd7a879ac9a1bb0ed1a34361b90deab
JasonY0615 <42446925+JasonY0615@users.noreply.github.com>
2018-09-19 15:28:33 +0800

    Update Calculate.java

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -32,4 +32,93 @@ public class Calculate {
 		int fraction = denominator * wholenumber + numerator; 
 		return fraction + "/" + denominator; 
 	}
+	// return max of the double passed
+	public static double max(double x, double y) {
+		if(x>y) {
+			return x;
+		}
+		else {
+			return y;
+		}
+	}
+	// return max from three doubles
+	public static double max(double x, double y, double z) { 
+		if (x>y && x>z) { 
+			return x; 
+		}
+		else if(y>x && y>z){
+			return y;
+		}
+		else {
+			return z;
+		}
+	}
+	// return min from passed two int
+	public static int min(int x, int y) { 
+		if (x<y) { 
+			return x; 
+		}
+		else {
+			return y; 
+		}
+	}
+	// return round 2
+	public static double round2(double x) {
+		x = Math.round(x*100.0)/100.0;
+		return x;
+	}
+	// exponent 
+	
+	public static double exponent(double x, int y) {
+		double temp = x;
+		for(int i = 1; i < y; i++ ) {
+			x = x*temp;
+		}
+		return x;
+	}
+	
+	public static int factorial(int x) {
+		for(int i = x; i > 0; i--) {
+			x = x*i;
+		}
+	}
+	
+	public static boolean isPrime(int x) {
+		int [] y = new int[x];
+		for(int i = 1; i <= x; i++) {
+			if(isDivisible(x, i) == true) {
+				y[i] = 1;
+			}
+			else {
+				y[i] = 0;
+			}
+		}
+		
+		int sum=0;
+		for(int i = 1; i < x; i++) {
+			sum = sum + i;
+		}
+		
+		if(sum == 2) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static int gcf(int x, int y) {
+		int min = min(x, y);
+		int max = 0;
+		for(int i = 1; i <= min; i++) {
+			if(isDivisible(x, i) && isDivisible(y, i)) {
+				if(i > max) {
+					max = i;
+				}
+			}
+		}
+
+	return max;
+	}
+	
+}
 }
